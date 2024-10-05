fbe35b6c3128735fae4b86a617c1c082e119f2a5
supra-matucasal <matias.casal@yahoo.com.ar>
2024-10-05 11:21:56 +0200

    Remove app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CountriesModule } from './modules/countries/countries.module';
 import { StringsModule } from './modules/strings/strings.module';
 
 @Module({
   imports: [CountriesModule, StringsModule],
-  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
